b1534972e0274e0c8f61b29254af0add406c0dc0
thewebtud <thewebtud@gmail.com>
2022-11-28 22:30:01 +0530

    Deploy React app to GitHub Pages

---
+++ precache-manifest.23e33b0b9dd75b837eff61fcd15f3c7d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "c788920c2a89f3bb8d19",
+    "url": "/asset-management-app/static/css/main.520e9f87.chunk.css"
+  },
+  {
+    "revision": "c788920c2a89f3bb8d19",
+    "url": "/asset-management-app/static/js/main.c788920c.chunk.js"
+  },
+  {
+    "revision": "0394700ef78f8875da09",
+    "url": "/asset-management-app/static/css/1.b14bb1ee.chunk.css"
+  },
+  {
+    "revision": "0394700ef78f8875da09",
+    "url": "/asset-management-app/static/js/1.0394700e.chunk.js"
+  },
+  {
+    "revision": "4b33144028c5e0bd479a",
+    "url": "/asset-management-app/static/js/runtime~main.4b331440.js"
+  },
+  {
+    "revision": "64ef1b6602d407596eabc53cc3c99d9a",
+    "url": "/asset-management-app/static/media/annalect_logo.64ef1b66.webp"
+  },
+  {
+    "revision": "f39a0d9743ba5c4753b51e0ee7c4039e",
+    "url": "/asset-management-app/static/media/Omnicom_Group_Logo.f39a0d97.webp"
+  },
+  {
+    "revision": "479999b26a50d410af7f9c9bc779dd36",
+    "url": "/asset-management-app/index.html"
+  }
+];
